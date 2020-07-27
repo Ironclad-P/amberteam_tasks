622a6763b40ca104441de4c260f67e11952fe068
Ironclad-P <65358100+Ironclad-P@users.noreply.github.com>
2020-07-27 12:34:43 +0200

    Update test_2.py
    assert fix - "Ok. Good answer"

--- Test_cases/test_2.py
+++ Test_cases/test_2.py
@@ -24,7 +24,7 @@ def test_2(app_config):
         button_Check.click()
         sleep(1)
 
-        assert result.text == 'OK. Good answerv'
+        assert result.text == 'OK. Good answer'
     finally:
         driver.close()
         driver.quit()
